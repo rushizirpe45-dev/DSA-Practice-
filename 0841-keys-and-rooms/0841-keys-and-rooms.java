6c82f23dda189986c717ada8b90e19e58d9a2f4b
Rushikesh Zirpe <rushikeshzirpe4444@gmail.com>
2026-09-08 18:25:33 +0530

    09-08-2026 0841-keys-and-rooms

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,28 +1,28 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        int n=rooms.size();
-        boolean[] ver=new boolean[n];
-        Queue<Integer> q=new LinkedList<>();
+        int n = rooms.size();
+        boolean[] ver = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
         q.add(0);
-        ver[0]=true;
-        while(q.size()>0){
-            int front=q.remove();
-            List<Integer> list=rooms.get(front);
-            for(int i=0;i<list.size();i++){
-                int a=list.get(i);
-                if(!ver[a]){
+        ver[0] = true;
+        while (q.size() > 0) {
+            int front = q.remove();
+            List<Integer> list = rooms.get(front);
+            for (int i = 0; i < list.size(); i++) {
+                int a = list.get(i);
+                if (!ver[a]) {
                     q.add(a);
-                    ver[a]=true;
+                    ver[a] = true;
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            if(!ver[i]){
+        for (int i = 0; i < n; i++) {
+            if (!ver[i]) {
                 return false;
             }
         }
 
         return true;
-        
+
     }
 }
