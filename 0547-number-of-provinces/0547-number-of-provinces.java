3c7e735df35cb63065f23a05b971756258dd4246
Rushikesh Zirpe <rushikeshzirpe4444@gmail.com>
2026-09-09 21:07:40 +0530

    09-09-2026 0547-number-of-provinces

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,25 +1,26 @@
 class Solution {
-    public void Dfs(int i,boolean[] vis,int [][] adj){
-        int n=adj.length;
-        vis[i]=true;
-        for(int j=0;j<n;j++){
-            if(adj[i][j]==1 && vis[j]!=true){
-                Dfs(j,vis,adj);
+    public void Dfs(int i, boolean[] vis, int[][] adj) {
+        int n = adj.length;
+        vis[i] = true;
+        for (int j = 0; j < n; j++) {
+            if (adj[i][j] == 1 && vis[j] != true) {
+                Dfs(j, vis, adj);
             }
         }
-       
+
     }
+
     public int findCircleNum(int[][] adj) {
-        int n=adj.length;
-        int count=0;
-        boolean[] visited=new boolean[n];
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
+        int n = adj.length;
+        int count = 0;
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
                 count++;
-                Dfs(i,visited,adj);
+                Dfs(i, visited, adj);
             }
         }
         return count;
-        
+
     }
 }
