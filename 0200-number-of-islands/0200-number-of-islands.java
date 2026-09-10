47cb7f41acccdda7ca03dfae546ffe4d4b8f548e
Rushikesh Zirpe <rushikeshzirpe4444@gmail.com>
2026-09-10 19:56:35 +0530

    09-10-2026 0200-number-of-islands

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,34 +1,49 @@
 class Solution {
-    public void Bfs(Pair<Integer,Integer> start, boolean[][] vis, char[][] grid) {
+    // public void Bfs(Pair<Integer,Integer> start, boolean[][] vis, char[][] grid) {
+    //     int rows = grid.length;
+    //     int cols = grid[0].length;
+    //     Queue<Pair<Integer,Integer>> q = new LinkedList<>();
+    //     q.add(start);
+    //     vis[start.getKey()][start.getValue()] = true;
+
+    //     while (q.size() > 0) {
+    //         Pair<Integer,Integer> front = q.remove();
+    //         int i = front.getKey();
+    //         int j = front.getValue();
+
+    //         if (j - 1 >= 0 && !vis[i][j-1] && grid[i][j-1] == '1') {
+    //             q.add(new Pair<>(i, j - 1));
+    //             vis[i][j-1] = true;
+    //         }
+    //         if (j + 1 < cols && !vis[i][j+1] && grid[i][j+1] == '1') {
+    //             q.add(new Pair<>(i, j + 1));
+    //             vis[i][j+1] = true;
+    //         }
+    //         if (i - 1 >= 0 && !vis[i-1][j] && grid[i-1][j] == '1') {
+    //             q.add(new Pair<>(i - 1, j));
+    //             vis[i-1][j] = true;
+    //         }
+    //         if (i + 1 < rows && !vis[i+1][j] && grid[i+1][j] == '1') {
+    //             q.add(new Pair<>(i + 1, j));
+    //             vis[i+1][j] = true;
+    //         }
+    //     }
+    // }
+
+
+
+    public void dfs(int i,int j, boolean[][] vis, char[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
-        Queue<Pair<Integer,Integer>> q = new LinkedList<>();
-        q.add(start);
-        vis[start.getKey()][start.getValue()] = true;
+        vis[i][j]=true;
+        if (j - 1 >= 0 && !vis[i][j-1] && grid[i][j-1] == '1') dfs(i,j-1,vis,grid);
+        if (j + 1 < cols && !vis[i][j+1] && grid[i][j+1] == '1') dfs(i,j+1,vis,grid);
+        if (i - 1 >= 0 && !vis[i-1][j] && grid[i-1][j] == '1') dfs(i-1,j,vis,grid);
+        if (i + 1 < rows && !vis[i+1][j] && grid[i+1][j] == '1') dfs(i+1,j,vis,grid);
 
-        while (q.size() > 0) {
-            Pair<Integer,Integer> front = q.remove();
-            int i = front.getKey();
-            int j = front.getValue();
 
-            if (j - 1 >= 0 && !vis[i][j-1] && grid[i][j-1] == '1') {
-                q.add(new Pair<>(i, j - 1));
-                vis[i][j-1] = true;
-            }
-            if (j + 1 < cols && !vis[i][j+1] && grid[i][j+1] == '1') {
-                q.add(new Pair<>(i, j + 1));
-                vis[i][j+1] = true;
-            }
-            if (i - 1 >= 0 && !vis[i-1][j] && grid[i-1][j] == '1') {
-                q.add(new Pair<>(i - 1, j));
-                vis[i-1][j] = true;
-            }
-            if (i + 1 < rows && !vis[i+1][j] && grid[i+1][j] == '1') {
-                q.add(new Pair<>(i + 1, j));
-                vis[i+1][j] = true;
-            }
-        }
     }
+
 
     public int numIslands(char[][] grid) {
         int rows = grid.length;
@@ -40,7 +55,8 @@ class Solution {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == '1' && !vis[i][j]) {
                     count++;
-                    Bfs(new Pair<>(i, j), vis, grid);
+                    // Bfs(new Pair<>(i, j), vis, grid);
+                    dfs(i, j, vis, grid);
                 }
             }
         }
